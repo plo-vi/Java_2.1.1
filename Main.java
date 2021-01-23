be8abf4b4c6c9ea85f020a5dc5863ddefd617883
viktoriia287 <75584040+viktoriia287@users.noreply.github.com>
2021-01-23 13:42:46 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketSell = 15000;
+        int milesNumber = ticketSell / 20;
+
+        System.out.println(milesNumber);
+    }
+}
