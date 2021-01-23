d98f678025a76538168f5769e3e7da996614bf63
viktoriia287 <75584040+viktoriia287@users.noreply.github.com>
2021-01-23 21:31:30 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        double ticketSellKop = 125_000_000.50;
+        int ticketSellKop = 12500_50;
 
         if (ticketSellKop < 0) {
             ticketSellKop = 0;
         }
 
-        double milesNumber = ticketSellKop / 20;
-
-        System.out.printf("%.2f", milesNumber);
+        int milesNumber = ticketSellKop / 20 / 100;
+        System.out.println(milesNumber);
     }
 }
-
-// version 2 of realisation the task
-//
-// public class Main {
-//     public static void main(String[] args) {
-//         int ticketSellKop = 12500_50;
-// 
-//         if (ticketSellKop < 0) {
-//             ticketSellKop = 0;
-//         }
-//
-//         int milesNumber = ticketSellKop / 20 / 100;
-// 
-//         System.out.println(milesNumber);
-//     }
-// }
